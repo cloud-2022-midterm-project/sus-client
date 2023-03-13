dc80401338e2a46fc2f468ffebefda85a2c8ad4d
Sukrit Banthao <arm.xdsk@gmail.com>
2023-03-13 18:10:55 +0700

    client script with clean up

---
+++ client_with_clean_up.py
@@ -0,0 +1,7 @@
+import os
+import glob
+import app
+app.sync()
+for file in glob.glob('cached_mutations_*'):
+    os.remove(file)
+os.path.exists('cached_posts.csv') and os.remove('cached_posts.csv')
